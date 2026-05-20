d09c469a86a31983dcbb26e54d49c99e2c285f2f
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-05-20 08:35:48 -0300

    Add RemoteEvent for network honeypot monitoring
    Implement network monitoring for player events

--- main.lua
+++ main.lua
@@ -1,1 +1,41 @@
+ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
 
+local MonitorEvent = Instance.new ("RemoteEvent")
+MonitorEvent.Name = "NetworkHoneypotEvent"
+MonitorEvent.Parent = ReplicatedStorage
+ -- ClientReplicator e ServerReplicator
+-- RemoteEvent é uma ilusão. forçar isso para o honeypot
+local playerData = {}
+local JANELA_DE_TEMPO = 0.1
+local LIMITE_DE_PACOTES = 5
+
+Players.PlayerAdded.Connect(funtion(player)
+  playerData[player.UserId] = {
+    last_time = os.clock(),
+    pacotesAcumulados = 0 
+  } --struct zerada das informações de pacotes do player, clientreplicator based
+end)
+
+Players.PlayerRemoving:Connect(function(player)
+  playerData[player.UserId] = nil
+end)
+
+MonitorEvent.OnServerEvent:Connect(function(player)
+local dados = playerData[player.UserdId]
+  if not dados then return end -- player indefinido ou bugado, da pra colocar uma função desconectando ele e pedindo para relogar, até que os dados dele sejam passados ao servidor
+
+  local tempoAtual = os.clock()
+  local deltaTempo = tempoAtual - dados.last_time
+
+  if deltaTempo <= JANELA_DE_TEMPO then
+    dados.pacotesAcumulados += 1
+    if dados.pacotesAcumulados > LIMITE_DE_PACOTES
+      warn (string.format("ta com flag ou bugado"))
+      warn (string.format("%d pacotes recebidos em %.3f segundos", dados.pacotesAcumulados, deltaTempo))
+      -- :Kick()
+      dados.pacotesAcumulados = 1
+      dados.last_time = tempoAtual
+    end
+
+end)
